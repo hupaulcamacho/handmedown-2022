8c1e81a92b08597230054428634e426556d092cf
Hupaul <hupaulcamacho@pursuit.org>
2022-04-10 22:54:20 -0400

    create simple express server with server running at portt 3002

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+require("dotenv");
+
+
+// import controllers
+
+
+// implement controllers
+
+
+// app listening at port
+app.listen(3002, () => {
+    console.log("Handmedown server is running at port 3002")
+})
