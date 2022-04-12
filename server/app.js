9d80beedd291d9837524d2c5a0acc030449b4ac3
Hupaul <hupaulcamacho@pursuit.org>
2022-04-12 01:25:49 -0400

    add controller to route and begin making dummy json data for database

--- server/app.js
+++ server/app.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
 require("dotenv");
-
+const PORT = process.env.PORT;
 
 // import controllers
-
+const usersController = require("./controllers/usersController");
 
 // implement controllers
-
+app.use("/users", usersController);
 
 // app listening at port
 app.listen(3002, () => {
-    console.log("Handmedown server is running at port 3002")
+    console.log("Handmedown server is running at port 3002");
 })
